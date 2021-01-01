190618788e28fd049a0943787f987229e1dfe84f
kos-san <makocchya917@gmail.com>
2021-01-01 16:40:49 +0900

    commentsテーブルのカラムの設定

---
+++ db/migrate/20210101073600_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :name,    null: false
+      t.string :email,   null: false
+      t.string :title,   null: false
+      t.text   :content, null: false
+
+      t.timestamps
+    end
+  end
+end
