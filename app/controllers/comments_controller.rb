009ba7cd67e740d0bfe5c04c864a239f483335a5
kos-san <makocchya917@gmail.com>
2021-01-02 23:46:36 +0900

    タイムゾーンを日本時間に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,13 +7,11 @@ class CommentsController < ApplicationController
   end
 
   def create
-    binding.pry
     Comment.create(comment_params)
   end
 
   private
   def comment_params
-    binding.pry
     params.require(:comment).permit(:name, :email, :title, :content)
   end
 end
