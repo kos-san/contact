301be851b75d06d7cbfd2decaba5520ec3fd0fea
kos-san <makocchya917@gmail.com>
2021-01-02 23:30:54 +0900

    フォーム送信機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,18 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.order("id DESC")
   end
-
-
-
   def new
+    @comment = Comment.new
+  end
+
+  def create
+    binding.pry
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    binding.pry
+    params.require(:comment).permit(:name, :email, :title, :content)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
